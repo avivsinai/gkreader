0bee0909b820af93452453b50ca5813979834ca2
aviv.s <avivsinai@gmail.com>
2013-04-19 07:22:36 +0300

    Playing around with jquery-validate

--- coming_soon.js
+++ coming_soon.js
@@ -1,11 +1,45 @@
 if (Meteor.isClient) {
   Template.coming_soon.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+    'click #subscribeBtn': function (event, template) {
+      var emailElem = $(".email");
+
+      if (!isSubscriberValid(emailElem)) {
+        console.log("Error in validation");
+        emailElem.addClass("error");
+        return;
+      }
+
+      if (emailElem.hasClass("error")) {
+        console.log("emailElem with error");
+        return; // Do nothing...
+      } 
+
+      console.log("EmailElem without error");
+      Meteor.call("addSubscriber", emailElem.val(), function (error, subscriber) {
+        if (error) {
+          console.log("error is " + error.details + ", " + error.reason);
+          emailElem.addClass("error");
+        }
+      });
+  }});
+
+  function isSubscriberValid(emailElem) {
+    return $("subscribeForm").validate(
+    {
+      rules: {
+        email: {
+          email: true,
+          required: true
+        }
+      },
+      highlight: function(element) {
+        $(element).removeClass("success").addClass("error");
+      },
+      success: function(element) {
+        element.text("ok");
+      }
+     }).form();
+  }
 }
 
 if (Meteor.isServer) {
