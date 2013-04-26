7a750e0531a6e8eaa1993eb9911ea1529fca686b
aviv.s <avivsinai@gmail.com>
2013-04-26 17:45:21 +0300

    Added some files

---
+++ model.js
@@ -0,0 +1,17 @@
+// Model definitions would reside here
+Subscribers = new Meteor.Collection("subscribers");
+// { _id: subscriber_id, email: String, validation_date: Date}
+
+// Model server methods available in the client
+Meteor.methods({
+	addSubscriber: function(subscriberEmail) {
+		console.log("SubEmail is " + subscriberEmail);
+		if (!subscriberEmail) {
+			throw new Meteor.Error(400, "Please provide a valid email address");
+		}
+		return Subscribers.insert({ 
+					email: subscriberEmail, 
+					is_valid: false, 
+					validation_date: null});
+	}
+});
