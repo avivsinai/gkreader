7bb736db9ed1aa8cff13129e8c62cc3f5c59fe65
aviv.s <avivsinai@gmail.com>
2013-05-02 22:21:02 +0300

    Actually centering the main div

--- model.js
+++ model.js
@@ -6,9 +6,13 @@ Subscribers = new Meteor.Collection("subscribers");
 Meteor.methods({
 	addSubscriber: function(subscriberEmail) {
 		console.log("SubEmail is " + subscriberEmail);
+
 		if (!subscriberEmail) {
 			throw new Meteor.Error(400, "Please provide a valid email address");
 		}
+
+		
+
 		return Subscribers.insert({ 
 					email: subscriberEmail, 
 					is_valid: false, 
