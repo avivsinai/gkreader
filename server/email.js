4839c09a19e5ebd485708d258d20188aca648fca
aviv.s <avivsinai@gmail.com>
2013-05-19 11:07:30 +0300

    Added new files and deps

---
+++ server/email.js
@@ -0,0 +1,17 @@
+Meteor.methods({
+	sendEmail: function (to, from, subject, text, html) {
+		// Let other method calls from the same client start running,
+	    // without waiting for the email sending to complete.
+	    this.unblock();
+
+	    console.log("Sending email with parameters: " + to + "||" + from + "||" + subject + "||" + text + "||" + html);
+
+	    Email.send({
+	      to: to,
+	      from: from,
+	      subject: subject,
+	      text: text,
+	      html: html
+		});
+	}
+});
